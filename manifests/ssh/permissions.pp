5d4d1442b5b3c443d311f1a08e351f1d16ff61cf
Tarik Achoughi <114010458+secfit@users.noreply.github.com>
2025-03-17 10:33:06 +0000

    Update permissions.pp

--- manifests/ssh/permissions.pp
+++ manifests/ssh/permissions.pp
@@ -1,4 +1,5 @@
 class linux_hardening::ssh::permissions {
+  # Set permissions for /etc/ssh/sshd_config
   file { '/etc/ssh/sshd_config':
     ensure  => file,
     mode    => '0600',
@@ -6,7 +7,24 @@ class linux_hardening::ssh::permissions {
     group   => 'root',
   }
 
-  file { '/etc/ssh':
+  # Ensure permissions on SSH private keys
+  file { '/etc/ssh/ssh_host_*_key':
+    ensure  => file,
+    mode    => '0600',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+  # Ensure permissions on SSH public keys
+  file { '/etc/ssh/ssh_host_*_key.pub':
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+  # Set permissions for SSH config directory
+  file { '/etc/ssh/sshd_config.d':
     ensure  => directory,
     mode    => '0700',
     owner   => 'root',
