bd42d1730eaa91055176f577129d776f3b0b9412
Tarik Achoughi <114010458+secfit@users.noreply.github.com>
2025-03-17 10:23:10 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
-class cis_ssh_hardening {
-  include cis_ssh_hardening::permissions
-  include cis_ssh_hardening::sshd_config
+class linux_hardening {
+  include linux_hardening::cron
+  include linux_hardening::ssh
+  include linux_hardening::sudo
+  include linux_hardening::pam
+  include linux_hardening::password_policy
 }
